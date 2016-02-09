def69140e65ad305cad03ea860b27f64ed462086
Matt Ward <ward.matt@gmail.com>
2016-02-09 18:23:00 +0000

    Add project.

---
+++ MyLib/MyClass.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace MyLib
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+
+        public string SayHello()
+        {
+#if X64
+            return "Hello x64";
+#else
+            return "Hello x86";
+#endif
+        }
+    }
+}
+
